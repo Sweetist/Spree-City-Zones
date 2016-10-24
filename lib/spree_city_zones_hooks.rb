8ff5d3d777ee71efef6dc79d9367c76b3d88148c
enpingan <enpingan@gmail.com>
2016-10-24 20:41:33 +0300

    change override class name

--- lib/spree_city_zones_hooks.rb
+++ lib/spree_city_zones_hooks.rb
@@ -1,4 +1,4 @@
-class SpreeCityZonesHooks < Spree::BaseController
+class SpreeCityZonesHooks < Spree::Core
   # custom hooks go here
   insert_after :admin_configurations_menu, 'shared/cities_admin_configurations_menu'
 end
